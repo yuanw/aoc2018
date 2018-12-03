66377cceb9bef911721cd258e8f5b7c482ffaa42
Yuan Wang <yuan.w0811@gmail.com>
2018-12-02 23:04:17 -0600

    WIP

---
+++ notes/Day01.md
@@ -0,0 +1,5 @@
+
+
+https://github.com/yuanw/fp-course/blob/yuan-answer/src/Course/State.hs#L134
+
+[`findM`](https://hackage.haskell.org/package/extra-1.6.13/docs/Control-Monad-Extra.html#v:findM)
--- src/Dice.hs
+++ src/Dice.hs
@@ -18,6 +18,7 @@ clumsyRollDice = (n, m)
         (n, g) = randomR (1,6) (mkStdGen 0)
         (m, _) = randomR (1,6) g
 
+-- Use our own version for now
 newtype State s a = State { runState :: s -> (a, s) }
 
 instance Functor (State s) where
@@ -27,3 +28,36 @@ instance Applicative (State s) where
     pure a = State (\ s -> (a, s))
     (State h) <*> (State g) = State (\ s -> let (f, s') = h s
                                                 (a, s'') = g s' in (f a, s''))
+
+instance Monad (State s) where
+    (State g) >>= mf = State (\ s -> let (a, s') = g s
+                                         (b, s'') = runState (mf a) s' in (b, s''))
+
+state :: (s -> (a, s)) -> State s a
+state = State
+
+put :: s -> State s ()
+put newState = state $ \_ -> ((), newState)
+
+get :: State s s
+get = state $ \s -> (s, s)
+
+evalState :: State s a -> s -> a
+evalState p s = fst (runState p s)
+
+execState :: State s a -> s -> s
+execState p s = snd (runState p s)
+
+-- -- The StdGen type we are using is an instance of RandomGen.
+-- randomR :: (Random a, RandomGen g) => (a, a) -> g -> (a, g)
+-- randomR = undefined
+
+
+rollDie :: State StdGen Int
+rollDie = state $ randomR (1, 6)
+
+rollDie' :: State StdGen Int
+rollDie' = do generator <- get
+              let (value, newGenerator) = randomR (1,6) generator
+              put newGenerator
+              return value
